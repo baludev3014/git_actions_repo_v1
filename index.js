bae1c3d29c986219afa3c96992b160033b333f64
baludev3014 <kbalu3013@gmail.com>
2026-04-19 19:30:01 +0530

    changes into .gitignore and added new route related to user

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/new',(req,res)=>{
     res.send("New Route")
 })
 
+app.get('/user/:userID',(req,res)=>{
+    res.send(`User ID : ${req.params.userID}`)
+})
+
 app.listen(4000,()=>{
     console.log("Server is listening on port 4000")
 })
